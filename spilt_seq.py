9c824012c71f53db8eb8c74e3acff4f9a507c279
endeavor-zzc <3125644551@qq.com>
2023-08-19 16:07:12 +0800

    update

--- spilt_seq.py
+++ spilt_seq.py
@@ -12,18 +12,20 @@ def spilt_seq(path, left_path, right_path):
     if not os.path.exists(right_path):
         os.makedirs(right_path)
     filenames = os.listdir(path)
-    print(filenames)
+    # print(filenames)
     for filename in filenames:
         dir = path + '/' + filename
         left_data = []
         right_data = []
         with open(dir, 'r') as f:
-            # left_data.append(f.read(15))
-            left_data.append(f.read(29))
+            # left_data.append(f.read(29))  # 有空格
+            left_data.append(f.read(15))
             f.close()
         with open(dir, 'r') as f1:
-            # right_data.append(f1.read()[16:31])
-            right_data.append(f1.read()[32:61])
+            # right_data.append(f1.read()[32:61])  # 有空格
+            right_data.append(f1.read()[16:31])
+            # right_data = right_data.reverse()
+            # print(right_data)
             f1.close()
             with open(left_path + '/' + filename, 'w') as file:
                 str1 = str(left_data)
@@ -31,8 +33,7 @@ def spilt_seq(path, left_path, right_path):
                 str1 = str1.replace(']', '')
                 str1 = str1.replace('\'', '')
                 str1 = str1.replace('\'', '')
-                # str1 = str1.rstrip()
-                str1 = str1[::-1]
+                # str1 = str1[::-1]
                 file.write(str1)
                 file.close()
             with open(right_path + '/' + filename, 'w') as file:
@@ -41,35 +42,12 @@ def spilt_seq(path, left_path, right_path):
                 str1 = str1.replace(']', '')
                 str1 = str1.replace('\'', '')
                 str1 = str1.replace('\'', '')
-                # str1 = str1.rstrip()
-                # str1 = str1[::-1]
-                file.write(str1)
-                file.close()
 
-def spilt(path, path_new): # 倒序全序列
-    if not os.path.exists(path_new):
-        os.makedirs(path_new)
-    filenames = os.listdir(path)
-    print(filenames)
-    for filename in filenames:
-        dir = path + '/' + filename
-        all_data = []
-        with open(dir, 'r') as f1:
-            # right_data.append(f1.read()[16:31])
-            all_data.append(f1.read())
-            f1.close()
-            with open(path_new + '/' + filename, 'w') as file:
-                str1 = str(all_data)
-                str1 = str1.replace('[', '')
-                str1 = str1.replace(']', '')
-                str1 = str1.replace('\'', '')
-                str1 = str1.replace('\'', '')
-                # str1 = str1.rstrip()
-                str1 = str1[::-1]
                 file.write(str1)
                 file.close()
 
 
 if __name__ == '__main__':
-    spilt('RLL/neg', 'neg')
-    spilt('RLL/pos', 'pos')
+    spilt_seq('neg', 'left_neg', 'right_neg')
+    spilt_seq('pos', 'left_pos', 'right_pos')
+    print('down')
